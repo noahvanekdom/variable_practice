d6c3b18c83603395f04985be49ac4f2881640d6a
Noah van Ekdom <noahvanekdom@gmail.com>
2022-06-07 15:35:26 -0600

    Check variable validity

--- variables.rb
+++ variables.rb
@@ -10,3 +10,11 @@ preferred_ph = 6.72
 multiple = true
 requires_fertilizer = true
 bee_friendly = false
+species = "Pinus thunbergii"
+common_name = "Black Pine"
+soil = "sandy"
+puts soil
+puts age
+puts heightm-preferred_ph
+puts species
+puts bee_friendly
